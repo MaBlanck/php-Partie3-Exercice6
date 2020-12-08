f1916c3387bd23df17813946c4e9b319d8f7aa2b
Max <maxblanck@outlook.fr>
2020-12-08 15:55:34 +0100

    php Partie 3  Exercice 6

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Exercice 6 partie 3</title>
+</head>
+<body>
+    <h1>Exercice 6 Partie 3</h1>
+
+    <ul>
+    <?php 
+    for($number = 20; $number >= 0; $number--)
+    {
+        echo '<li>' . 'C\'est presque bon.' . '</li>';
+    }
+    ?></ul>
+</body>
+</html>
